bb7b22930259b3611b608845fb2e4f9138cfc5b3
Mohamad Omran <mohamad1_3omran@hotmail.com>
2021-03-05 02:20:02 +0200

    darkmode toggle functionality

---
+++ src/Utils/DarkModeToggle.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+ Box,
+ IconButton,
+ Tooltip,
+ useColorMode,
+ useColorModeValue,
+ useTheme,
+} from '@chakra-ui/react';
+import { FiMoon, FiSun } from 'react-icons/fi';
+
+export const DarkModeToggle: React.FC = () => {
+ const theme = useTheme();
+ const { colorMode, toggleColorMode } = useColorMode();
+
+ const label = useColorModeValue('Enable Dark Mode', 'Enable Light Mode');
+ return (
+  <Box>
+   <Tooltip label={label} hasArrow>
+    {colorMode === 'light' ? (
+     <IconButton
+      aria-label="enable dark mode"
+      variant="ghost"
+      colorScheme={theme.colors.red[500]}
+      color="black"
+      icon={<FiMoon />}
+      onClick={toggleColorMode}
+     />
+    ) : (
+     <IconButton
+      aria-label="enable light mode"
+      variant="ghost"
+      colorScheme={theme.colors.red[500]}
+      color="white"
+      icon={<FiSun />}
+      onClick={toggleColorMode}
+     />
+    )}
+   </Tooltip>
+  </Box>
+ );
+};
